30a6fd0f04de68fa3ef5b08a550a4a1c30e65e0a
Clément B <clem69.b@gmail.com>
2025-11-01 11:55:31 +0100

    [Invoice] Fix Amplify deployment

--- amplify/functions/generate-invoice-pdf/resource.ts
+++ amplify/functions/generate-invoice-pdf/resource.ts
@@ -17,19 +17,27 @@ export const generateInvoicePdf = defineFunction(
       description: "fonts and libs required by weasyprint",
     });
 
+    /*
+      * Layer for other dependencies required by generate-invoice-pdf function
+      * This layer includes packages that are not part of the weasyprint layer,
+      * such as reportlab, boto3, and others.
+      *
+      * Obtained by this following script:
+      *
+      *   mkdir -p layer/python
+      *   cp requirements.txt layer/
+      *   docker run --rm -v "$PWD/layer":/var/task public.ecr.aws/sam/build-python3.12 \
+      *     pip install -r requirements.txt -t python
+      *   cd layer
+      *   zip -r ../dependenciesLayer.zip python
+      */
+
     const dependenciesLayer = new LayerVersion(scope, "dependenciesLayer", {
-      code: Code.fromAsset(functionDir, {
-        bundling: {
-          image: Runtime.PYTHON_3_12.bundlingImage,
-          command: [
-            "bash",
-            "-c",
-            "pip install -r requirements.txt -t /asset-output/python",
-          ],
-        },
-      }),
+      code: Code.fromAsset(
+        path.join(functionDir, "dependenciesLayer.zip")
+      ),
       compatibleRuntimes: [Runtime.PYTHON_3_12],
-      description: "Python dependencies from requirements.txt",
+      description: "Python dependencies for generate-invoice-pdf function",
     });
 
     return new Function(scope, "generate-invoice-pdf", {
